c0fffcc66ef9226ad3ae81a9c0fce1ca5afa6686
Angry <69483036+Angry123946@users.noreply.github.com>
2022-11-15 21:22:51 +0100

    fix move(rather)

--- Assets/Scripts/Birdspawner.cs
+++ Assets/Scripts/Birdspawner.cs
@@ -9,7 +9,7 @@ public class Birdspawner : MonoBehaviour
     [SerializeField] private int i = 10;
 
     [SerializeField] private int co_ile_dowac;
-    [SerializeField] private Vector3 pos = new Vector3(100, 0, Random.Range(1, 10));
+    [SerializeField] private Vector3 pos = new Vector3(100, 0, 0);
 
     public void Gnerate()
     {
@@ -23,5 +23,9 @@ public class Birdspawner : MonoBehaviour
     void Start()
     {
         InvokeRepeating("Gnerate", 0, 0.4f);
+    }
+    private void Awake()
+    {
+        pos = new Vector3(100,  Random.Range(1, 10), 0);
     }
 }
